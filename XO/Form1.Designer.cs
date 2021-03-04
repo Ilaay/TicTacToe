7adbcd11b7bf01a28ef1608f6c3a7c3692c64219
Ilaay <52674359+Ilaay@users.noreply.github.com>
2021-03-04 11:41:10 +0200

    TicTacToe v.1

---
+++ XO/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b11 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b33 = new System.Windows.Forms.Button();
+            this.boxTurn = new System.Windows.Forms.RichTextBox();
+            this.boxError = new System.Windows.Forms.RichTextBox();
+            this.bRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b11
+            // 
+            this.b11.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b11.Location = new System.Drawing.Point(12, 12);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(126, 122);
+            this.b11.TabIndex = 0;
+            this.b11.UseVisualStyleBackColor = true;
+            this.b11.Click += new System.EventHandler(this.b11_Click);
+            // 
+            // b12
+            // 
+            this.b12.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b12.Location = new System.Drawing.Point(165, 12);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(126, 122);
+            this.b12.TabIndex = 1;
+            this.b12.UseVisualStyleBackColor = true;
+            this.b12.Click += new System.EventHandler(this.b12_Click);
+            // 
+            // b13
+            // 
+            this.b13.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b13.Location = new System.Drawing.Point(315, 12);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(126, 122);
+            this.b13.TabIndex = 2;
+            this.b13.UseVisualStyleBackColor = true;
+            this.b13.Click += new System.EventHandler(this.b13_Click);
+            // 
+            // b21
+            // 
+            this.b21.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b21.Location = new System.Drawing.Point(12, 152);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(126, 122);
+            this.b21.TabIndex = 3;
+            this.b21.UseVisualStyleBackColor = true;
+            this.b21.Click += new System.EventHandler(this.b21_Click);
+            // 
+            // b22
+            // 
+            this.b22.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b22.Location = new System.Drawing.Point(165, 152);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(126, 122);
+            this.b22.TabIndex = 4;
+            this.b22.UseVisualStyleBackColor = true;
+            this.b22.Click += new System.EventHandler(this.b22_Click);
+            // 
+            // b23
+            // 
+            this.b23.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b23.Location = new System.Drawing.Point(315, 152);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(126, 122);
+            this.b23.TabIndex = 5;
+            this.b23.UseVisualStyleBackColor = true;
+            this.b23.Click += new System.EventHandler(this.b23_Click);
+            // 
+            // b31
+            // 
+            this.b31.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b31.Location = new System.Drawing.Point(12, 293);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(126, 122);
+            this.b31.TabIndex = 6;
+            this.b31.UseVisualStyleBackColor = true;
+            this.b31.Click += new System.EventHandler(this.b31_Click);
+            // 
+            // b32
+            // 
+            this.b32.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b32.Location = new System.Drawing.Point(165, 293);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(126, 122);
+            this.b32.TabIndex = 7;
+            this.b32.UseVisualStyleBackColor = true;
+            this.b32.Click += new System.EventHandler(this.b32_Click);
+            // 
+            // b33
+            // 
+            this.b33.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b33.Location = new System.Drawing.Point(315, 293);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(126, 122);
+            this.b33.TabIndex = 8;
+            this.b33.UseVisualStyleBackColor = true;
+            this.b33.Click += new System.EventHandler(this.b33_Click);
+            // 
+            // boxTurn
+            // 
+            this.boxTurn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.boxTurn.Location = new System.Drawing.Point(471, 35);
+            this.boxTurn.Name = "boxTurn";
+            this.boxTurn.ReadOnly = true;
+            this.boxTurn.Size = new System.Drawing.Size(249, 42);
+            this.boxTurn.TabIndex = 9;
+            this.boxTurn.Text = "First player\'s turn";
+            // 
+            // boxError
+            // 
+            this.boxError.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.boxError.ForeColor = System.Drawing.Color.LimeGreen;
+            this.boxError.Location = new System.Drawing.Point(471, 83);
+            this.boxError.Name = "boxError";
+            this.boxError.ReadOnly = true;
+            this.boxError.Size = new System.Drawing.Size(249, 92);
+            this.boxError.TabIndex = 10;
+            this.boxError.Text = "";
+            // 
+            // bRestart
+            // 
+            this.bRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bRestart.Location = new System.Drawing.Point(546, 328);
+            this.bRestart.Name = "bRestart";
+            this.bRestart.Size = new System.Drawing.Size(174, 69);
+            this.bRestart.TabIndex = 11;
+            this.bRestart.Text = "Restart";
+            this.bRestart.UseVisualStyleBackColor = true;
+            this.bRestart.Click += new System.EventHandler(this.bRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 424);
+            this.Controls.Add(this.bRestart);
+            this.Controls.Add(this.boxError);
+            this.Controls.Add(this.boxTurn);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b11);
+            this.Name = "Form1";
+            this.Text = "XO";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.RichTextBox boxTurn;
+        private System.Windows.Forms.RichTextBox boxError;
+        private System.Windows.Forms.Button bRestart;
+    }
+}
+
---
+++ XO/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO
+{
+    public partial class Form1 : Form
+    {
+        string Turn = "X";
+        bool somebodyAlreadyWon = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void DisableControls(Control con)
+        {
+            foreach (Control c in con.Controls)
+            {
+                DisableControls(c);
+            }
+            con.Enabled = false;
+        }
+
+        private void TurnSwap()
+        {
+            if (Turn == "X")
+            {
+                Turn = "O";
+            }
+            else
+                Turn = "X";
+        }
+        private void ButtonPressed()
+        {
+            TurnSwap();
+
+            if (boxTurn.Text == "First player's turn")
+            {
+                boxTurn.Text = "Second player's turn";
+            }
+            else
+                boxTurn.Text = "First player's turn";
+        }
+
+        private void SomebodyWon()
+        {
+            if (b11.Text != "" && b11.Text == b22.Text && b22.Text == b33.Text )
+            {
+                ButtonPressed();
+                boxError.Text = "Player "+Turn+" won!";
+                somebodyAlreadyWon = true;
+            }
+            else if (b31.Text != "" && b31.Text == b22.Text && b22.Text == b13.Text)
+            {
+                ButtonPressed();
+                boxError.Text = "Player " + Turn + " won!";
+                somebodyAlreadyWon = true;
+            }
+            else if(b11.Text != "" && b11.Text == b12.Text && b12.Text == b13.Text)
+            {
+                ButtonPressed();
+                boxError.Text = "Player " + Turn + " won!";
+                somebodyAlreadyWon = true;
+            }
+            else if (b21.Text != "" && b21.Text == b22.Text && b22.Text == b23.Text)
+            {
+                ButtonPressed();
+                boxError.Text = "Player " + Turn + " won!";
+                somebodyAlreadyWon = true;
+            }
+            else if (b13.Text != "" && b13.Text == b23.Text && b23.Text == b33.Text)
+            {
+                ButtonPressed();
+                boxError.Text = "Player " + Turn + " won!";
+                somebodyAlreadyWon = true;
+            }
+            else if (b31.Text != "" && b31.Text == b32.Text && b32.Text == b33.Text)
+            {
+                ButtonPressed();
+                boxError.Text = "Player " + Turn + " won!";
+                somebodyAlreadyWon = true;
+            }
+            else if (b11.Text != "" && b11.Text == b21.Text && b21.Text == b31.Text)
+            {
+                ButtonPressed();
+                boxError.Text = "Player " + Turn + " won!";
+                somebodyAlreadyWon = true;
+            }
+        }
+
+        private void b11_Click(object sender, EventArgs e)
+        {
+            if (b11.Text == "" && somebodyAlreadyWon == false)
+            {
+                b11.Text = Turn;
+                ButtonPressed();
+                SomebodyWon();
+            }
+        }
+
+        private void b12_Click(object sender, EventArgs e)
+        {
+            if (b12.Text == "" && somebodyAlreadyWon == false)
+            {
+                b12.Text = Turn;
+                ButtonPressed();
+                SomebodyWon();
+            }
+        }
+
+        private void b13_Click(object sender, EventArgs e)
+        {
+            if (b13.Text == "" && somebodyAlreadyWon == false)
+            {
+                b13.Text = Turn;
+                ButtonPressed();
+                SomebodyWon();
+            }
+        }
+
+        private void b21_Click(object sender, EventArgs e)
+        {
+            if (b21.Text == "" && somebodyAlreadyWon == false)
+            {
+                b21.Text = Turn;
+                ButtonPressed();
+                SomebodyWon();
+            }
+        }
+
+        private void b22_Click(object sender, EventArgs e)
+        {
+            if (b22.Text == "" && somebodyAlreadyWon == false)
+            {
+                b22.Text = Turn;
+                ButtonPressed();
+                SomebodyWon();
+            }
+        }
+
+        private void b23_Click(object sender, EventArgs e)
+        {
+            if (b23.Text == "" && somebodyAlreadyWon == false)
+            {
+                b23.Text = Turn;
+                ButtonPressed();
+                SomebodyWon();
+            }
+        }
+
+        private void b31_Click(object sender, EventArgs e)
+        {
+            if (b31.Text == "" && somebodyAlreadyWon == false)
+            {
+                b31.Text = Turn;
+                ButtonPressed();
+                SomebodyWon();
+            }
+        }
+
+        private void b32_Click(object sender, EventArgs e)
+        {
+            if (b32.Text == "" && somebodyAlreadyWon == false)
+            {
+                b32.Text = Turn;
+                ButtonPressed();
+                SomebodyWon();
+            }
+        }
+
+        private void b33_Click(object sender, EventArgs e)
+        {
+            if (b33.Text == "" && somebodyAlreadyWon == false)
+            {
+                b33.Text = Turn;
+                ButtonPressed();
+                SomebodyWon();
+            }
+        }
+
+        private void bRestart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
